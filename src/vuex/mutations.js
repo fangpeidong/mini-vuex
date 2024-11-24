9d77349173e160bfdaea29403598aed60f3b8d15
fangpeidong <1062027862@qq.com>
2024-11-25 01:18:58 +0800

    chore: 删除错误的注释

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -4,15 +4,12 @@ export function setupMutations(store, mutations) {
   store.mutations = {}
 
   forEachValue(mutations, (key, func) => {
-    // 监听方法 （把回调函数推入目标事件的监听函数队列里去）
     store.mutations[key] = payload => {
       func.call(store, store.state, payload)
     }
   })
 
-  //订阅方法
   store.commit = (type, payload) => {
-    // 逐个调用队列里的回调函数
     store.mutations[type](payload)
   }
 }
